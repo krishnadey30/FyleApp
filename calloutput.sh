ac2f58fdd45084e5d449c3e047e0edf56507d47d
krishnadey30 <krishnakumardey.dey@gmail.com>
2019-07-26 01:49:41 +0530

    Adds Curl  Script

--- calloutput.sh
+++ calloutput.sh
@@ -1,3 +1,74 @@
 #! /bin/sh
-output=`curl --header "Authorization: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzuYW1lIjoia3Jpc2huYSIsImV4cCI6MTU2NDUwMjEwOX0.U8tnl9KmXEDbDfETCC2bqcPP52b2sTUTBvqKWH7lGrA" -H "username: krishna" -G "https://bankapp-fyle.herokuapp.com/branch" --data-urlencode "bank_name=STATE BANK OF INDIA" --data-urlencode "city=RANCHI"`
+## variables
+token="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoia3Jpc2huYSIsImV4cCI6MTU2NDUwMjEwOX0.U8tnl9KmXEDbDfETCC2bqcPP52b2sTUTBvqKWH7lGrA"
+url="https://bankapp-fyle.herokuapp.com/"
+bank_name="STATE BANK OF INDIA"
+city="RANCHI"
+offset=2
+limit=5
+ifsc="SBIN0006355"
+
+echo "\n\t\t\tTASK: Fyle BE Coding Challenge: Banks Search Application (v1)\n"
+echo "\t\t\t\tSubmitted BY: Krishna Kumar Dey"
+
+echo "======================================================================================================\
+=====================================\n\n\n"
+echo "\t\tValidating GET API to fetch all details of branches, given bank name and a city"
+echo "\t\t_______________________________________________________________________________\n"
+echo "Case I: Using the default offset\n"
+echo "**********************************************************************\n"
+echo "Bank Name: $bank_name"
+echo "City     : $city"
+echo "Default Offset = 0\nDefault LIMIT = 20\n"
+echo "\nRESULT:\n"
+
+## GET API to fetch all details of branches, given bank name and a city 
+output=`curl -s --header "Authorization: $token" -H "username: krishna" -G "${url}branch" \
+        --data-urlencode "bank_name=$bank_name" --data-urlencode "city=$city"`
+
 echo $output
+echo "\n************************************************************************\n\n"
+
+echo "Case II: Changing the offset and LIMIT\n"
+echo "************************************************************************\n"
+echo "Bank Name: $bank_name"
+echo "City     : $city"
+echo "OFFSET   : $offset"
+echo "LIMIT    : $limit"
+echo "\nRESULT:\n"
+
+## GET API to fetch all details of branches, given bank name and a city 
+output=`curl -s --header "Authorization: $token" -H "username: krishna" -G "${url}branch"\
+         --data-urlencode "bank_name=$bank_name" --data-urlencode "city=$city"\
+        --data-urlencode "offset=$offset" --data-urlencode "limit=$limit"`
+
+echo $output
+
+echo "======================================================================================================\
+=====================================\n\n\n"
+echo "\t\tValidating GET API to fetch a bank details, given branch IFSC code"
+echo "\t\t__________________________________________________________________\n"
+echo "IFSC     : $ifsc"
+echo "\nRESULT:\n"
+## GET API to fetch a bank details, given branch IFSC code
+output=`curl -s --header "Authorization: $token" -H "username: krishna" -G "${url}bank" --data-urlencode "ifsc=$ifsc"`
+
+echo $output
+
+echo "======================================================================================================\
+=====================================\n"
+
+echo "======================================================================================================\
+=====================================\n\n\n"
+echo "\t\tGenerating a Token, given a username"
+echo "\t\t____________________________________\n"
+username="krishna"
+echo "username     : $username"
+echo "\nRESULT:\n"
+## GET API to fetch a bank details, given branch IFSC code
+output=`curl -s -G "${url}generate" --data-urlencode "name=$username"`
+
+echo $output
+
+echo "======================================================================================================\
+=====================================\n"
